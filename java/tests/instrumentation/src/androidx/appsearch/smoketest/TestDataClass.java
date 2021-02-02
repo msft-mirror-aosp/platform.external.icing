252f6145fd47c0818bb7d7afb8ad2edffde0d250
Jie Song <jies@google.com>
2021-02-02 21:41:35 +0000

    Revert "Update AppSearchSmokeTest to match latest AppSearch API."
    Revert "Add appsearch-platform-storage to targets synced by upda..."
    
    Revert submission 13452382-appsearch_jetpack_sync-2021-02-01
    
    Reason for revert: <b/179119910>
    Reverted Changes:
    Iebd6d70e0:Import AndroidX from build 7116071
    I3ca516967:Update AppSearchSmokeTest to match latest AppSearc...
    I40fcdb169:Add appsearch-platform-storage to targets synced b...
    
    Change-Id: I62a01b4ba1585208fdc92440d1114a51b850070a
    (cherry picked from commit 1d5658d179b3dba118488fa2d1313d6766759b24)

--- java/tests/instrumentation/src/androidx/appsearch/smoketest/TestDataClass.java
+++ java/tests/instrumentation/src/androidx/appsearch/smoketest/TestDataClass.java
@@ -16,17 +16,17 @@
 
 package androidx.appsearch.smoketest;
 
-import androidx.appsearch.annotation.Document;
-import androidx.appsearch.app.AppSearchSchema.StringPropertyConfig;
+import androidx.appsearch.annotation.AppSearchDocument;
+import androidx.appsearch.app.AppSearchSchema.PropertyConfig;
 
-@Document
-public class TestDocument {
-    @Document.Uri private final String uri;
+@AppSearchDocument
+public class TestDataClass {
+    @AppSearchDocument.Uri private final String uri;
 
-    @Document.Property(indexingType = StringPropertyConfig.INDEXING_TYPE_PREFIXES)
+    @AppSearchDocument.Property(indexingType = PropertyConfig.INDEXING_TYPE_PREFIXES)
     private final String body;
 
-    TestDocument(String uri, String body) {
+    TestDataClass(String uri, String body) {
         this.uri = uri;
         this.body = body;
     }
