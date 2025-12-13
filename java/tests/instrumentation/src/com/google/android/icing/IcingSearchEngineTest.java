e97998201564a9dc712fb7af9ab536c9f881b268
Tim Barron <tjbarron@google.com>
2025-12-13 01:56:06 +0000

    Update Icing from upstream.
    Descriptions:
    ========================================================================
    Ignore test writeAndReadBlob_blobContentMatches because it uses reflection.
    ========================================================================
    
    Test: presubmit
    Change-Id: Ia09ab99a4e3a51dacf4414b774343a3fe12c8868

--- java/tests/instrumentation/src/com/google/android/icing/IcingSearchEngineTest.java
+++ java/tests/instrumentation/src/com/google/android/icing/IcingSearchEngineTest.java
@@ -745,6 +745,7 @@ public final class IcingSearchEngineTest {
   }
 
 
+  @Ignore // b/350530146
   @Test
   public void writeAndReadBlob_blobContentMatches() throws Exception {
     // 1 Arrange: set up IcingSearchEngine with and blob data
